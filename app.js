1e41765ffbda1507768bd005a47fdca6f881ce08
Brian00412 <brianfernandez004@gmail.com>
2023-08-01 00:56:03 -0300

    s1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(8080, ()=>{
     console.log(
         "%cservidor funcionando",
         `font-size: 24px;
