6e8c8fb09243b11b04aa8756b8a61bc0da895e72
GabrielM004 <gabrielmullicundo04@gmail.com>
2023-08-03 13:30:42 -0300

    login-register

--- app.js
+++ app.js
@@ -20,3 +20,18 @@ app.get('/', (req,res)=>{
 app.get("/productDetail", (req, res)=>{
     res.sendFile(__dirname + "/views/productDetail.html");
 });
+app.get("/login", (req, res)=>{
+    res.sendFile(__dirname + "/views/login.html");
+});
+app.get("/register", (req, res)=>{
+    res.sendFile(__dirname + "/views/register.html");
+});
+app.get("/registerr", (req, res)=>{
+    res.sendFile(__dirname + "/views/registerr.html");
+});
+app.get('/contraolvidada', (req, res) => {
+    res.sendFile(__dirname + '/views/contraolvidada.html');
+  });
+app.get("/botones.html", (req, res)=>{
+    res.sendFile(__dirname + "/views/botones.html");
+});
