d48dcbef4f84e35ec342a99415c22507ed79651d
Félix <boca13409@gmail.com>
2023-08-17 21:38:26 -0300

    solucion de app.use

--- src/app.js
+++ src/app.js
@@ -4,10 +4,7 @@ const routerProduct = require('./routes/productsR');
 const app = express();
 app.use(express.static('./public'));
 app.set("view engine", "ejs");
-app.set("views", "src/views/products");
 app.set("views", "src/views");
-
-
 
 app.listen(8080, ()=>{
     console.log(
--- src/controllers/productsC.js
+++ src/controllers/productsC.js
@@ -1,12 +1,12 @@
 const productController = {
     detalles: (req, res) => {
-        res.render("productDetail");
+        res.render("products/productDetail");
     },
     crear: (req, res) => {
-        res.render("crearProduct")
+        res.render("products/crearProduct")
     },
     editar: (req, res) => {
-        res.render("editarProducto")
+        res.render("products/editarProducto")
     },
     create: (req, res) => {
         let nuevoProducto = {
