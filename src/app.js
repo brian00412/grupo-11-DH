f175a25f826762cf5f9c92acd9831f40fab2c682
Félix <boca13409@gmail.com>
2023-08-24 19:09:39 -0300

    Formulario

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const router = require('./routes');
 const routerProduct = require('./routes/productsR');
 const app = express();
 app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", "src/views");
 
@@ -17,7 +19,12 @@ app.listen(8080, ()=>{
 });
 
 app.use('/', router);
+app.use((req, res, next)=>{
+    console.log(req.method);
+    console.log(req.body);
+    next()
 
+})
 app.use("/", routerProduct)
 
 app.get('/contraolvidada', (req, res) => {
--- src/routes/productsR.js
+++ src/routes/productsR.js
@@ -3,10 +3,11 @@ const productController = require("../controllers/productsC");
 const routerProduct = express.Router();
 
 routerProduct.get("/productDetail", productController.detalles);
-routerProduct.get("/crearProduct", productController.crear);
-routerProduct.get("/editarProducto", productController.editar);
 
+routerProduct.get("/crearProduct", productController.crear);
 routerProduct.post("/crearProduct", productController.create);
+
+routerProduct.get("/editarProducto", productController.editar);
 
 
 module.exports = routerProduct;
