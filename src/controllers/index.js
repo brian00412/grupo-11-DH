eb35110214c181dd214d7f1266ff7cf1e815a6d4
Brian00412 <brianfernandez004@gmail.com>
2023-09-01 16:55:25 -0300

    multer medio

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,7 +4,35 @@ const productspath = path.join(__dirname, "../data/productos.json")
 const products = JSON.parse(fs.readFileSync(productspath))
 
 
+const getProducts = (req, res) => {
+    res.status(200).json(usuario)
+}
 
+const personasController = {
+   
+    crear: (req, res) => {
+        res.render("register")
+    },
+
+    create: (req, res) => {
+        console.log(req.file);
+        let nuevopersonas = {
+            id: Date.now(),
+            Nombreyapellido: req.body.nombreYapellido,
+            Nombredeusuari: req.body.nombre_usuario,
+            Cumpleaño: req.body.Cumpleaño,
+            foto_usuario: req.body.foto_usuario,
+            password: req.body.password,
+            
+        };
+
+        usuario.push(nuevopersona);
+        fs.writeFileSync(productspath, JSON.stringify(usuario));
+        res.redirect("/")
+
+    },
+    
+}
 
 const homecontroller= (req, res) => {
    res.render('home',{products})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const { homecontroller, registercontroller, logincontroller, productCartcontroller } = require('../controllers');
+const multer = require('multer');
+
 
 
 
 const router = express.Router();
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/images/personas");
+    },
+    filename: (req, file, cb) => {
+        const extension = file.originalname.split(".")[1];
+        cb(null, file.fieldname + "-" + Date.now() + `.${extension}`)
+    }
+})
 
 
 router.get('/', homecontroller);
