aa0411220528ca6d89f6b9b83cf6816b67a9208d
Brian00412 <brianfernandez004@gmail.com>
2023-11-30 20:08:55 -0300

    Data Base Scrip arreglados 2

--- database/basededatoCODE/grupo_11_datos (5).sql
+++ database/basededatoCODE/grupo_11_datos (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-11-2023 a las 23:33:50
+-- Tiempo de generación: 30-11-2023 a las 23:48:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
