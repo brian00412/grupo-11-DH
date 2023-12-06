f2fd63a24c43417af09c59cc3a93728bd2be3977
Brian00412 <brianfernandez004@gmail.com>
2023-12-05 21:16:48 -0300

    areglo de login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,34 +7,18 @@ const bcrypt = require("bcryptjs")
 const productspath = path.join(__dirname, "../data/productos.json");
 const products = JSON.parse(fs.readFileSync(productspath));
 const db = require('../../database/models');
-
-
 const { validationResult } = require('express-validator');
 const { raw } = require('mysql2');
-///////////////// zona de pruebas //////////////////
 
-
-// db.User.findAll( {raw: true})
-// .then ((datos) => {
-//     console.log ('soy yo datos users')
-//      console.log(datos)
-//     //  const datosRay = datos.map(function (user){
-//     //  })
-//     //  console.log(datos) 
-// }) ;
-
-
-
-////////////////////////////////////////////////////
 
 const usersController = {
-    StoreUser : (req, res) => {
+    StoreUser: (req, res) => {
         let error = validationResult(req);
         res.send(error)
-    if (!error.isEmpty()){
-        return res.render("register",{mensajesDeError : error.mapped()})
-    } 
-},
+        if (!error.isEmpty()) {
+            return res.render("register", { mensajesDeError: error.mapped() })
+        }
+    },
 
     crear: (req, res) => {
         res.render("register")
@@ -50,46 +34,38 @@ const usersController = {
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password),
             category: req.body.categoria,
-            image: req.file ? req.file.filename : 'Sinfoto.jpg', 
+            image: req.file ? req.file.filename : 'Sinfoto.jpg',
         };
 
         const validation = validationResult(req);
 
         if (!validation.isEmpty()) {
-        console.log(validation.mapped())
-            res.render ('register',{
-                errors : validation.mapped()})
-            return;};
-
-        
-
+            console.log(validation.mapped())
+            res.render('register', {
+                errors: validation.mapped()
+            })
+            return;
+        };
         try {
-            await db.User.create(newUser) 
+            await db.User.create(newUser)
             console.log();
             res.render('home', { products });
-         } catch (error) {
+        } catch (error) {
             console.log(error);
             res.status(500).send('error');
-         }   
-
-         
+        }
         // console.log(newUser);
         // users.push(newUser);
         // fs.writeFileSync(userspath, JSON.stringify(users));
         // res.render("home", { products })
 
     },
-
-
     // Login controller
-
     logincontroller: (req, res) => {
         res.render('login')
 
     },
-
     loginvalidar: (req, res) => {
-        
         const gmail = req.body.email
         const contra = req.body.password
         let userperfil = false
@@ -97,32 +73,39 @@ const usersController = {
         const validationuser = validationResult(req);
 
         if (!validationuser.isEmpty()) {
-        console.log(validationuser.mapped())
-            res.render ('login',{
-                errors : validationuser.mapped()})
-            return;};
-////       terminar usuario
-        for (let i = 0; i < datouser.length; i++) {
-            if (datouser[i].email === gmail) {
-            } if (comparedatouser[i].password === contra) {
-                userperfil = true;
-                break;
-            } 
-                
-            
-        }
-        if (userperfil){
-            res.redirect("home");
-        }
-        else{
-            res.send("el usurio no existe")
-        }
+            console.log(validationuser.mapped())
+            res.render('login', {
+                errors: validationuser.mapped()
+            })
+            return;
+        };
+        ////       terminar usuario
+        db.User.findAll({ raw: true })
+            .then((User) => {
+                User.forEach((usuarios) => { console.log(usuarios)
+                    if (usuarios.email == gmail) {
+                        const comprador = bcrypt.compareSync(contra, usuarios.password)
+                        if (comprador == true) {
+                            userperfil = true
+                        }
+                    }
+                })
+                console.log (userperfil)
+                if (userperfil) {
+                    res.redirect("/");
+                }
+                else {
+                    res.send("el usurio no existe")
+                }
+            });
     }
-
-
-
 }
 
+
+
+// let user =  User.findOne({
+//     where: {email:req.body.email}
+// })
 
 
 
