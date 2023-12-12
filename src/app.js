e9e56e338bb9cf364ce2197097ffdff4f6a05a8e
Brian00412 <brianfernandez004@gmail.com>
2023-12-12 01:30:09 -0300

    home

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
